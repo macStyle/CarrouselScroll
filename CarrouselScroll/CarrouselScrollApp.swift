a5c333023c7aad006dc795682395aa17556654e3
Adam <theoutsider009@gmail.com>
2022-06-11 12:57:20 +0200

    Initial Commit

---
+++ CarrouselScroll/CarrouselScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrouselScrollApp.swift
+//  CarrouselScroll
+//
+//  Created by Adam Jemni on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CarrouselScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
